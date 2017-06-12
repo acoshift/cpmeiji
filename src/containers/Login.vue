62663b1e2697f9ce5eab497b1b4cb93d4fd7f09a
BioMaRu <Bio.Color.Palette@gmail.com>
2017-06-12 22:42:59 +0700

    initial

---
+++ src/containers/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login _bg-color-accent">
+    <div class="grid-container">
+      <div class="_flex-column _full-width">
+        <div class="row">
+
+          <div class="col-xs-12 _align-center cp-block-bigger">
+            <img class="logo" src="~@/assets/meiji.png" width="100%">
+          </div>
+
+          <div class="col-xs-12 col-md-6 col-md-offset-3">
+            <div class="cp-card cp-segment _flex-column">
+
+              <div class="cp-header _font-sub _color-sub _align-center">
+                Sign In
+              </div>
+
+              <div class="input-field _flex-column">
+                <label>Username:</label>
+                <input class="cp-input" name="Email" placeholder="Username">
+              </div>
+
+              <div class="input-field _flex-column">
+                <label>Password:</label>
+                <input class="cp-input" name="Email" placeholder="Password">
+              </div>
+
+              <div class="_flex-row _main-center">
+                <button class="cp-button -primary _font-sub cp-block _full-width" type="submit">
+                  Sign In
+                </button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .login {
+    padding-top: 48px;
+    width: 100%;
+    height: 100vh;
+    box-sizing: border-box;
+  }
+  .logo {
+    max-width: 200px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/containers/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/login',
+      name: 'Hello',
+      component: Login
+    },
+    {
+      path: '/intensive',
+      name: 'Hello',
+      component: Hello,
+      children: [
+        {
+          path: 'checkin',
+          component: Hello
+        }
+      ]
+    },
+    {
+      path: '/order',
       name: 'Hello',
       component: Hello
-    }
+    },
+    { path: '*', redirect: '/login' }
   ]
 })
