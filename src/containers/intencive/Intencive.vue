4083801e61669750622e48937f36fab92ebf0014
BioMaRu <Bio.Color.Palette@gmail.com>
2017-06-12 22:59:31 +0700

    initial

--- src/containers/intencive/Intencive.vue
+++ src/containers/intencive/Intencive.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
+    <intencive-navbar></intencive-navbar>
     Intencive
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import IntenciveNavbar from './IntenciveNavbar'
+export default {
+  name: 'Intencive',
+  components: {
+    IntenciveNavbar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
---
+++ src/containers/intencive/IntenciveNavbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar _bg-color-accent">
+    Intencive Navbar
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.navbar {
+  height: 60px;
+}
+</style>
